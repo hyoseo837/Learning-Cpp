555d2e4e6e5be2a6d5cc55fbf991abf4a2b3f4ac
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2022-06-29 17:56:59 +0200

    Day 8

--- Gravitation/NewtonFractal.cpp
+++ Gravitation/NewtonFractal.cpp
@@ -5,9 +5,9 @@ class complexNum
 {
 private:
 public:
-    float real;
-    float imaginary;
-    complexNum (float realN, float imginN)
+    double real;
+    double imaginary;
+    complexNum (double realN, double imginN)
     {
     real = realN;
     imaginary = imginN;
@@ -41,30 +41,30 @@ complexNum Ctime(complexNum a, complexNum b) {
 }
 
 complexNum Cmult(complexNum a, float b) {
-    complexNum foo(a.real*b, a.imaginary*b);
+    complexNum foo(a.real * b, a.imaginary * b);
     return foo;
 }
+ 
+int coloring(double i, double j, double roots[3][2]) {
 
-int coloring(int i, int j, int roots[3][2]) {
-
-    int pos[2] = { i,j };
-    int distances[3];
+    double pos[2] = { i,j };
+    double distances[3];
     for (int k = 0; k < 3; k++)
     {
         distances[k] = (roots[k][0] - i) * (roots[k][0] - i) + (roots[k][1] - j) * (roots[k][1] - j);
     }
-    if (distances[0] < distances[1]) {
-        if (distances[0] < distances[2]) {
-            return 0;
-        }
+    if (distances[0] < distances[1] and distances[0] < distances[2]) {
+        return 50;
     }
-    else
-    {
-        if (distances[1] < distances[2]) {
-            return 128;
-        }
+    if (distances[1] < distances[0] and distances[1] < distances[2]) {
+        return 150;
     }
-    return 245;
+    if (distances[2] < distances[0] and distances[2] < distances[1]) {
+        return 250;
+    }
+
+    std::cout << std::to_string(distances[0]) + "  " + std::to_string(distances[1]) + "  " + std::to_string(distances[2]) << std::endl;
+    return 0;
 }
 
 
@@ -73,38 +73,35 @@ int main()
     int t = 0;
     sf::RenderWindow window(sf::VideoMode(400, 400), "Newton's fractal!");
     int size = 400;
+    complexNum one(1, 0);
 
-    int roots[3][2] = { {100,200},{200,200},{300,200} };
+    double roots[3][2] = { {1,0},{-0.5,sqrt(3)/2},{0.5,-sqrt(3)/2}};
 
-    complexNum kkk(2, 2);
-    complexNum ppp(1, 3);
-    complexNum rrr(0, 0);
-    rrr = Ctime(kkk,ppp);
-
-    std::cout << kkk.print() << std::endl;
-    std::cout << ppp.print() << std::endl;
-    std::cout << rrr.print() << std::endl;
-
-    /*int pixels[400][400];
+    int pixels[400][400];
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++)
         {
-            pixels[i][j] = coloring(i, j, roots);
+            complexNum z(static_cast<float>(i - 200)/200, static_cast<float>(j - 200)/200);
+            complexNum grad(0,0);
+            grad = Ctime(Csub(Ctime(Ctime(z,z),z),one),Creciprocal(Cmult(Ctime(z,z), 3)));
+            z = Csub(z, grad);
+            pixels[i][j] = coloring(z.real, z.imaginary, roots);
+
         }
-    }*/
+    }
 
     sf::Image image;
     image.create(size, size, sf::Color::Yellow);
 
-    /*for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++)
         {
             image.setPixel(i, j, sf::Color(pixels[i][j], pixels[i][j], pixels[i][j], 255));
         }
-    }*/
-    image.setPixel(100, 200, sf::Color(255, 0, 0, 255));
-    image.setPixel(200, 200, sf::Color(0, 255, 0, 255));
-    image.setPixel(300, 200, sf::Color(0, 0, 255, 255));
+    }
+    image.setPixel(400, 200, sf::Color(255, 0, 0, 255));
+    image.setPixel(100, 373, sf::Color(0, 255, 0, 255));
+    image.setPixel(100, 27, sf::Color(0, 0, 255, 255));
 
     sf::Texture texture;
     texture.loadFromImage(image);
@@ -116,7 +113,7 @@ int main()
 
     while (window.isOpen())
     {
-        //window.setFramerateLimit(120);
+        window.setFramerateLimit(1);
         t++;
 
         sf::Event event;
