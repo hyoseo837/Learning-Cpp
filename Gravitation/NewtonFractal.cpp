e8d6c1a35dd6d90529b1d370f807951cc99af656
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2022-07-03 20:40:27 +0200

    day 12

--- Gravitation/NewtonFractal.cpp
+++ Gravitation/NewtonFractal.cpp
@@ -46,22 +46,30 @@ complexNum Cmult(complexNum a, float b) {
     return foo;
 }
  
-int coloring(double i, double j, double roots[3][2]) {
+int coloring(double i, double j, double roots[3][2], complexNum origin,float ratio) {
 
     double pos[2] = { i,j };
     double distances[3];
+    double odistances[3];
     for (int k = 0; k < 3; k++)
     {
         distances[k] = (roots[k][0] - i) * (roots[k][0] - i) + (roots[k][1] - j) * (roots[k][1] - j);
+    }
+    for (int k = 0; k < 3; k++)
+    {
+        odistances[k] = (roots[k][0] - origin.real) * (roots[k][0] - origin.real) + (roots[k][1] - origin.imaginary) * (roots[k][1] - origin.imaginary);
+    }
+    if (odistances[0] < 5/(ratio*ratio) or odistances[1] < 5/(ratio*ratio) or odistances[2] < 5 / (ratio * ratio)){
+        return 255;
     }
     if (distances[0] < distances[1] and distances[0] < distances[2]) {
         return 50;
     }
     if (distances[1] < distances[0] and distances[1] < distances[2]) {
-        return 150;
+        return 100;
     }
     if (distances[2] < distances[0] and distances[2] < distances[1]) {
-        return 250;
+        return 150;
     }
 
     std::cout << std::to_string(i) + ",  " + std::to_string(j) + " : " + std::to_string(distances[0]) + "  " + std::to_string(distances[1]) + "  " + std::to_string(distances[2]) << std::endl;
@@ -74,29 +82,32 @@ int main()
 
     // variables that you can change
     int size = 1000;
-    int zoom = 50;
-    int iteration = 5;
-    float position[2] = {0.1,0.5};
+    int ratio = 300;
+    int iteration = 50;
+    float position[2] = {0.5,0.5};
 
     // system, constant
     int t = 0;
     const complexNum one(1, 0);
 
-    double roots[3][2] = { {1,0},{-0.5,sqrt(3)/2},{0.5,-sqrt(3)/2}};
+    double roots[3][2] = { {1,0},{-0.5,sqrt(3)/2},{-0.5,-sqrt(3)/2}};
 
     int* pixels = new int[1000000]; // should be square of size
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++)
-        {
-            complexNum z(static_cast<float>(i - (size * position[0])) / zoom, static_cast<float>(j - (size * position[1])) / zoom);
+        {   
+            complexNum z(static_cast<float>(i - (size * position[0])) / ratio, static_cast<float>(j - (size * position[1])) / ratio);
+            complexNum o(z.real, z.imaginary);
             complexNum grad(0,0);
             for (int h = 0; h < iteration; h++) {
                 grad = Ctime(Csub(Ctime(Ctime(z, z), z), one), Creciprocal(Cmult(Ctime(z, z), 3)));
                 z = Csub(z, grad);
             }
            
-            pixels[i*size+j] = coloring(z.real, z.imaginary, roots);
-
+            pixels[i*size+j] = coloring(z.real, z.imaginary, roots,o,ratio);
+        }
+        if (i % 50 == 0) {
+            std::cout << std::to_string(i * 100 / size) + "% done " << std::endl;
         }
     }
     
